7c54eab6457604c3c817752d163bcb1cb6925369
kimrodriguezFINTECH <kimrodriguez756@gmail.com>
2024-04-27 12:47:44 -0700

    commit

--- .ipynb_checkpoints/crypto_wallet-checkpoint.py
+++ .ipynb_checkpoints/crypto_wallet-checkpoint.py
@@ -15,6 +15,7 @@ from bip44 import Wallet
 from web3 import Account
 from web3 import middleware
 from web3.gas_strategies.time_based import medium_gas_price_strategy
+from ethereum import generate_account
 
 ################################################################################
 # Wallet functionality
